91ac2fe117b38dd62b4d240904f9fbf6e188acdd
zjjott <zjjott@gmail.com>
2024-02-06 17:50:18 +0800

    Auto Reorder

--- xla/service/gpu/gpu_hlo_schedule.cc
+++ xla/service/gpu/gpu_hlo_schedule.cc
@@ -15,52 +15,6 @@ limitations under the License.
 
 #include "xla/service/gpu/gpu_hlo_schedule.h"
 
-#include <cstddef>
-#include <cstdint>
-#include <deque>
-#include <memory>
-#include <optional>
-#include <string>
-#include <utility>
-#include <vector>
-
-#include "absl/algorithm/container.h"
-#include "absl/container/flat_hash_map.h"
-#include "absl/container/flat_hash_set.h"
-#include "absl/log/check.h"
-#include "absl/log/log.h"
-#include "absl/status/status.h"
-#include "absl/strings/match.h"
-#include "absl/strings/numbers.h"
-#include "absl/strings/str_format.h"
-#include "absl/strings/str_split.h"
-#include "absl/strings/string_view.h"
-#include "xla/hlo/ir/hlo_computation.h"
-#include "xla/hlo/ir/hlo_input_output_alias_config.h"
-#include "xla/hlo/ir/hlo_instruction.h"
-#include "xla/hlo/ir/hlo_instructions.h"
-#include "xla/hlo/ir/hlo_opcode.h"
-#include "xla/hlo/ir/hlo_schedule.h"
-#include "xla/hlo/utils/hlo_query.h"
-#include "xla/service/buffer_value.h"
-#include "xla/service/gpu/backend_configs.pb.h"
-#include "xla/service/gpu/cublas_cudnn.h"
-#include "xla/service/gpu/gpu_schedule_postprocessing.h"
-#include "xla/service/gpu/model/analytical_latency_estimator.h"
-#include "xla/service/hlo_memory_scheduler.h"
-#include "xla/service/hlo_pass_pipeline.h"
-#include "xla/service/latency_hiding_scheduler.h"
-#include "xla/service/p2p_schedule_preparation.h"
-#include "xla/service/profile_guided_latency_estimator.h"
-#include "xla/shape.h"
-#include "xla/shape_util.h"
-#include "xla/status.h"
-#include "xla/statusor.h"
-#include "xla/stream_executor/device_description.h"
-#include "xla/util.h"
-#include "tsl/platform/env.h"
-#include "tsl/platform/errors.h"
-#include "tsl/platform/protobuf.h"
 
 namespace xla {
 namespace gpu {
@@ -238,22 +192,6 @@ StatusOr<HloSchedule> ScheduleGpuModuleWithMemoryScheduler(
                                             PostProcessSchedule));
 }
 
-// Latency hiding scheduler support.
-
-CanonicalAsyncOp GpuGetCanonicalAsyncOp(const HloInstruction& hlo) {
-  switch (hlo.opcode()) {
-    case HloOpcode::kSend:
-      return {HloOpcode::kAsyncStart, HloOpcode::kSend};
-    case HloOpcode::kSendDone:
-      return {HloOpcode::kAsyncDone, HloOpcode::kSend};
-    case HloOpcode::kRecv:
-      return {HloOpcode::kAsyncStart, HloOpcode::kRecv};
-    case HloOpcode::kRecvDone:
-      return {HloOpcode::kAsyncDone, HloOpcode::kRecv};
-    default:
-      return DefaultGetCanonicalAsyncOp(hlo);
-  }
-}
 
 SchedulerConfig GetSchedulerConfig(int64_t memory_limit) {
   SchedulerConfig config;
@@ -695,14 +633,28 @@ Status ScheduleGpuModule(HloModule* module, int64_t pointer_size,
   auto shape_size_in_bytes = [pointer_size](const Shape& shape) {
     return GetSizeOfShape(shape, pointer_size);
   };
+  const bool enable_linear_program_scheduler =
+      module->config()
+          .debug_options()
+          .xla_gpu_enable_linear_program_scheduler();
+
   HloPassPipeline pipeline("latency-hiding-scheduler");
+  
   auto scheduler_core = std::make_unique<DefaultSchedulerCore>(
       shape_size_in_bytes, async_tracker.get(), latency_estimator.get(),
       config);
-  pipeline.AddPass<LatencyHidingScheduler>(
+  
+
+  if (enable_linear_program_scheduler){
+    pipeline.AddPass<AutoReorderPass>(
+      std::move(latency_estimator), std::move(async_tracker),
+      std::move(scheduler_core),shape_size_in_bytes
+    );
+  }else{
+      pipeline.AddPass<LatencyHidingScheduler>(
       std::move(latency_estimator), std::move(async_tracker),
       std::move(scheduler_core), shape_size_in_bytes);
-
+  }
   TF_RETURN_IF_ERROR(pipeline.Run(module).status());
 
   HloPassPipeline postprocessing_pipeline("gpu-schedule-postprocessing");
@@ -767,5 +719,5 @@ int64_t GetSchedulerMemoryLimit(const HloModule* module,
   return limit;
 }
 
-}  // namespace gpu
+}
 }  // namespace xla
--- xla/service/gpu/gpu_hlo_schedule.h
+++ xla/service/gpu/gpu_hlo_schedule.h
@@ -16,8 +16,52 @@ limitations under the License.
 #ifndef XLA_SERVICE_GPU_GPU_HLO_SCHEDULE_H_
 #define XLA_SERVICE_GPU_GPU_HLO_SCHEDULE_H_
 
-#include "xla/hlo/ir/hlo_module.h"
+#include <cstddef>
+#include <cstdint>
+#include <deque>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/algorithm/container.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/strings/match.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_format.h"
+#include "absl/strings/str_split.h"
+#include "absl/strings/string_view.h"
+#include "xla/hlo/ir/hlo_computation.h"
+#include "xla/hlo/ir/hlo_input_output_alias_config.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_instructions.h"
+#include "xla/hlo/ir/hlo_opcode.h"
+#include "xla/hlo/ir/hlo_schedule.h"
+#include "xla/hlo/utils/hlo_query.h"
+#include "xla/service/buffer_value.h"
+#include "xla/service/gpu/backend_configs.pb.h"
+#include "xla/service/gpu/cublas_cudnn.h"
+#include "xla/service/gpu/gpu_schedule_postprocessing.h"
+#include "xla/service/gpu/model/analytical_latency_estimator.h"
+#include "xla/service/hlo_memory_scheduler.h"
+#include "xla/service/hlo_pass_pipeline.h"
+#include "xla/service/latency_hiding_scheduler.h"
+#include "xla/service/p2p_schedule_preparation.h"
+#include "xla/service/profile_guided_latency_estimator.h"
+#include "xla/shape.h"
+#include "xla/shape_util.h"
+#include "xla/status.h"
+#include "xla/statusor.h"
 #include "xla/stream_executor/device_description.h"
+#include "xla/util.h"
+#include "tsl/platform/env.h"
+#include "tsl/platform/errors.h"
+#include "tsl/platform/protobuf.h"
 
 namespace xla {
 namespace gpu {
@@ -37,6 +81,8 @@ int64_t GetSchedulerMemoryLimit(const HloModule* module,
 constexpr absl::string_view kFingerprintBeforeLHS = "fingerprint_before_lhs";
 
 }  // namespace gpu
+
+
 }  // namespace xla
 
 #endif  // XLA_SERVICE_GPU_GPU_HLO_SCHEDULE_H_
